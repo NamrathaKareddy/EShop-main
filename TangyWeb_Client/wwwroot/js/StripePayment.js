8497738aa28a4420104b3019ebb91bd11fd9cac0
Namartha Kareddy <namrathareddykareddy@gmail.com>
2025-01-20 14:13:28 -0500

    Initial commit

---
+++ TangyWeb_Client/wwwroot/js/StripePayment.js
@@ -0,0 +1,4 @@
+﻿redirectToCheckout = function (sessionId) {
+    var stripe = Stripe("pk_test_51NXVWqJ4eTuMqTYnNLTKlYgMqaAYYMcNY4wvJC2BQLC5wQcBcvVMGBrze0oDOoQ1oN9OUBJolJ5Pbp0Isp4XnTNC00BRg94Xur");
+    stripe.redirectToCheckout({ sessionId: sessionId });
+}
